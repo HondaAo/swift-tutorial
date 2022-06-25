a07c82381eff45aea5489b53cd22f5d78807dacd
Honda <hondao1207@gmail.com>
2022-06-25 19:17:13 +0900

    Initial Commit

---
+++ Shared/Nova_VideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nova_VideoApp.swift
+//  Shared
+//
+//  Created by 誉田碧 on 2022/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Nova_VideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
